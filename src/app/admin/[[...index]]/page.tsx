55c9a1077ac088e3b2214d06108b2961ba94aaf2
Pranav <p2012agarwal@gmail.com>
2026-06-24 22:58:30 +0530

    Fix metadata export error in Sanity Studio route
    Split studio page into server component (metadata exports) and
    client component (NextStudio render) to satisfy Next.js constraints.

--- src/app/admin/[[...index]]/page.tsx
+++ src/app/admin/[[...index]]/page.tsx
@@ -1,10 +1,7 @@
-"use client";
-
-import { NextStudio } from "next-sanity/studio";
-import config from "../../../../sanity.config";
-
 export { metadata, viewport } from "next-sanity/studio";
 
+import Studio from "./Studio";
+
 export default function AdminPage() {
-	return <NextStudio config={config} />;
+	return <Studio />;
 }
