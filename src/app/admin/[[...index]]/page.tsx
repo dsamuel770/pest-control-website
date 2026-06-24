59b9ba75e14f66351fb20fd6bf005de82c831d98
Pranav <p2012agarwal@gmail.com>
2026-06-24 22:57:48 +0530

    Fix Sanity Studio crash on /admin route in production
    Add required metadata and viewport exports from next-sanity/studio,
    which Next.js needs to avoid a server crash on the studio route.

--- src/app/admin/[[...index]]/page.tsx
+++ src/app/admin/[[...index]]/page.tsx
@@ -3,6 +3,8 @@
 import { NextStudio } from "next-sanity/studio";
 import config from "../../../../sanity.config";
 
+export { metadata, viewport } from "next-sanity/studio";
+
 export default function AdminPage() {
 	return <NextStudio config={config} />;
 }
